1c38fc0232cfaa1454995c444c12e12651c90895
ouyanghan <user@Ouyh-E450>
2016-07-22 17:36:14 +0800

    依赖关系，启动还有个神奇的问题，明天再看吧

--- src/main/java/club/hoy/weixin/events/EventHandler.java
+++ src/main/java/club/hoy/weixin/events/EventHandler.java
@@ -6,16 +6,14 @@ import club.hoy.base.exception.Exceptions;
 import club.hoy.weixin.EVENT_TYPE;
 import club.hoy.weixin.model.EventMessage;
 
-public abstract interface EventHandler
-{
-  public abstract EVENT_TYPE[] eventTypes();
+public abstract interface EventHandler {
+    public abstract EVENT_TYPE[] eventTypes();
 
-  public abstract String handle(EventMessage paramEventMessage, EVENT_TYPE paramEVENT_TYPE)
-    throws CodedException;
+    public abstract String handle(EventMessage paramEventMessage, EVENT_TYPE paramEVENT_TYPE) throws CodedException;
 
-  public static class ErrorCodes
-  {
-    private static final String MODULE_CODE = "weixin.event.EventHandler";
-    public static final ErrorCode CAN_NOT_FOUND_MATHED_EVENT_HANDLER = Exceptions.errorCode("weixin.event.EventHandler", "CAN_NOT_FOUND_MATHED_EVENT_HANDLER");
-  }
+    public static class ErrorCodes {
+        private static final String MODULE_CODE = "weixin.event.EventHandler";
+        public static final ErrorCode CAN_NOT_FOUND_MATHED_EVENT_HANDLER = Exceptions
+            .errorCode("weixin.event.EventHandler", "CAN_NOT_FOUND_MATHED_EVENT_HANDLER");
+    }
 }
